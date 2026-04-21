ea50a29825f27b26e12c068fc81061f0a77f82a8
Helge Stegemoen <helge@stegemoen.com>
2026-04-21 18:37:23 +0200

    Created Db Context and added it to build.services, then created the database and added the migration. Note: also fixed some errors, that was deliberately to the connection string.

---
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // base(options) => whatever options we configure ​here will be passed on to the base class of ​TP context. 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        /* To add this to the database */
+        /* 1. Open Package Manage Console
+         * 2. Add database: 
+         *      update-database
+         * 3. To add migration:
+         *      add-migration <MigrationName>
+         * 4. To add migration to database:
+         * update-database
+         */
+    }
+}
